d83ac73a8f3b8457090ab01ac44f1ef036effa03
Wouter Landuydt <wouter.landuydt@gmail.com>
2018-06-15 20:20:55 +0200

    :fire: clear

--- src/containers/Home.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { withRouteData } from "react-static";
-
-export default withRouteData(data => (
-  <div>
-    {console.log(data)}
-    <h1>Home</h1>
-  </div>
-));
--- src/contentful/config.js
+++
@@ -1,3 +0,0 @@
-export const spaceId = "9ivj8jmhqi2h";
-export const accessToken =
-  "9849ed38346490e4154abafe157f11ac1e5380be9538889252c8f80238138772";
